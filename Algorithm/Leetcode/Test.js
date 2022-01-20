890c3b9159127919630a26dbc1f6bbac803ca7ad
YeahDreamItPossible <yeahdreamitpossible@gmail.com>
2022-01-20 21:23:32 +0800

    最长回文字符串完善中心扩散算法

--- Algorithm/Leetcode/Test.js
+++ Algorithm/Leetcode/Test.js
@@ -1,46 +1,57 @@
 // 中心扩散法
 const longestPalindrome = function (s) {
   if (s.length <= 1) {
-    return s;
+    return s
   }
 
-  let maxlength = 1;
-  let str = s[0];
+  let maxlength = 1
+  let str = s[0]
 
-  let len = s.length;
+  let len = s.length
 
-  let left, right
+  let left, right, flag = false
   for (let i = 1; i < len; i++) {
     left = right = i
     while (left >= 0 && s[left] === s[i]) {
+      flag = true
       left--
     }
+
     while (right < len && s[right] === s[i]) {
+      flag = true
       right++
     }
-    
-    left -= 1
-    right += 1
 
-    while (left >= 0 && right < len && s[left] === s[right]) {
-      left -= 1
-      right += 1
+    console.log(i, left, right, s[left], s[right])
+    while (
+      left >= 0 &&
+      right < len &&
+      left !== right &&
+      s[left] === s[right]
+    ) {
+      flag = true
+      left--
+      right++
     }
 
-    if (right - left - 1 > maxlength) {
-      maxlength = right - left - 1
-      str = s.slice(left++, right--)
+    if (flag) {
+      right--
+      left++
+    }
+
+    if (right - left + 1 > maxlength) {
+      maxlength = right - left + 1
+      str = s.slice(left, right + 1)
+      console.log(left, right, i, str)
     }
   }
 
-  console.log(maxlength)
   console.log(str)
-  // console.log(dp)
 
-  return str;
+  return str
 };
 
 // const str = "aacabdkacaa"
 // const str = "cbabc"
-const str = "babad"
+const str = "cbbd"
 longestPalindrome(str)
