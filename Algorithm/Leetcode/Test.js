b15dcbafdc197808eaf6a2619ebc1767b88e66a6
YeahDreamItPossible <yeahdreamitpossible@gmail.com>
2022-01-20 17:57:41 +0800

    最长回文字符串新增中心扩散算法

--- Algorithm/Leetcode/Test.js
+++ Algorithm/Leetcode/Test.js
@@ -1,19 +1,4 @@
-const initArr = len => {
-  let arr = new Array(len)
-  let sub = new Array(len)
-
-  for (let i = 0; i < len; i++) {
-    sub[i] = false
-    arr[i] = ''
-  }
-
-  for (let i = 0; i < len; i++) {
-    arr[i] = [ ...sub ]
-  }
-
-  return arr
-}
-
+// 中心扩散法
 const longestPalindrome = function (s) {
   if (s.length <= 1) {
     return s;
@@ -23,28 +8,28 @@ const longestPalindrome = function (s) {
   let str = s[0];
 
   let len = s.length;
-  let dp = initArr(len)
 
-  for (let i = 0; i < len; i++) {
-    dp[i][i] = true;
-  }
-
+  let left, right
   for (let i = 1; i < len; i++) {
-    for (let j = 0; j < i; j++) {
-      if (s[i] !== s[j]) {
-        dp[j][i] = false;
-      } else {
-        if (i - j >= 3) {
-          dp[j][i] = dp[j + 1][i - 1];
-        } else {
-          dp[j][i] = true;
-        }
-      }
+    left = right = i
+    while (left >= 0 && s[left] === s[i]) {
+      left--
+    }
+    while (right < len && s[right] === s[i]) {
+      right++
+    }
+    
+    left -= 1
+    right += 1
 
-      while (dp[j][i] && i + 1 - j > maxlength) {
-        maxlength = i + 1 - j;
-        str = s.slice(j, i + 1);
-      }
+    while (left >= 0 && right < len && s[left] === s[right]) {
+      left -= 1
+      right += 1
+    }
+
+    if (right - left - 1 > maxlength) {
+      maxlength = right - left - 1
+      str = s.slice(left++, right--)
     }
   }
 
@@ -56,5 +41,6 @@ const longestPalindrome = function (s) {
 };
 
 // const str = "aacabdkacaa"
-const str = "cbabc"
+// const str = "cbabc"
+const str = "babad"
 longestPalindrome(str)
