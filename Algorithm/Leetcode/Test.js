e61966d358a89d88d4569ffa7131d20f2dd8373b
YeahDreamItPossible <yeahdreamitpossible@gmail.com>
2022-01-22 00:23:30 +0800

    新增不同路径Ⅱ题目

--- Algorithm/Leetcode/Test.js
+++ Algorithm/Leetcode/Test.js
@@ -1,14 +1,28 @@
-const initArr = (m, n) => {
+const initArr = (m, n, obstacleGrid) => {
   let arr = []
   
   for (let i = 0; i < m; i++) {
       arr[i] = []
       for (let j = 0; j < n; j++) {
-          if (
-              i === m - 1 ||
-              j === n -1
-          ) {
-              arr[i][j] = 1
+          if (i === m - 1) {
+              if (obstacleGrid[i][j] === 1) {
+                arr[i][j] = 0
+                for (let k = 0; k < j; k++) {
+                    arr[i][k] = 0
+                }
+              } else {
+                arr[i][j] = 1
+              }
+          }
+          else if (j === n - 1) {
+              if (obstacleGrid[i][j] === 1) {
+                arr[i][j] = 0
+                for (let k = 0; k < i; k++) {
+                    arr[k][j] = 0
+                }
+              } else {
+                arr[i][j] = 1
+              }
           }
           else {
               arr[i][j] = 0
@@ -20,28 +34,31 @@ const initArr = (m, n) => {
 }
 
 
-var uniquePaths = function(m, n) {
-  if (m === 1 || n === 1) {
-      return 1
-  }
+var uniquePathsWithObstacles = function(obstacleGrid) {
+  let m = obstacleGrid.length
+  let n = obstacleGrid[0].length
 
   let dp = initArr(m, n)
 
   let i = m - 2
-  
+  let j = n - 2
+
   while (i >= 0) {
-    let j = n - 2
-    while (j >= 0) {
-      dp[i][j] = dp[i + 1][j] + dp[i][j + 1]
-      j--
-    }
-    i--
+      j = n - 2
+      while (j >= 0) {
+          if(obstacleGrid[i][j] === 1) {
+              dp[i][j] = 0
+          } else {
+              dp[i][j] = dp[i][j + 1] + dp[i + 1][j]
+          }
+          j--
+      }
+      i--
   }
 
-  console.log(dp)
   return dp[0][0]
 }
 
-console.log(uniquePaths(3, 7))
+console.log(uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]]))
 
 
